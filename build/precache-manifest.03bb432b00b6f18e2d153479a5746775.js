12ff88c5fd06da3a192d0d040e588938c1f97241
John Cris Tayco <bugz3jc@gmail.com>
2020-09-30 02:12:07 +0900

    build 02

---
+++ build/precache-manifest.03bb432b00b6f18e2d153479a5746775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a47265de90a80003026da1cb7a92085",
+    "url": "/index.html"
+  },
+  {
+    "revision": "929e9eb33d403fe8f4a1",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "098b705131a9e7998f5d",
+    "url": "/static/js/2.cc9fd382.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.cc9fd382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929e9eb33d403fe8f4a1",
+    "url": "/static/js/main.e9b1a28d.chunk.js"
+  },
+  {
+    "revision": "b411b719029fcaa094eb",
+    "url": "/static/js/runtime-main.2e67e8b3.js"
+  }
+]);
